28b5d4d87a99c3a8c973dcf28d01941afd3dc67a
fathirdirejaa <pancasastra005@gmail.com>
2025-11-03 11:48:55 +0700

    membuat welcome.blade.php

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Project Berhasil Jalan 🎉</title>
+</head>
+<body>
+    <h1>Halo, Laravel kamu sudah jalan dengan MySQL!</h1>
+    <p>Lokasi project: C:\laragon\www\GitHub\rpl</p>
+</body>
+</html>
